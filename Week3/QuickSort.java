f94af0c44224895a4e84033e4f788c69f2f75412
Core-007 <144080167+Core-007@users.noreply.github.com>
2025-05-29 21:35:06 +0700

    Create QuickSort.java

---
+++ Week3/QuickSort.java
@@ -0,0 +1,26 @@
+package Week3;
+
+public class QuickSort {
+
+    public static void sort(int[] arr, int low, int high) {
+        if (low >= high) return;
+
+        int pivot = arr[(low + high) / 2];
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (arr[i] < pivot) i++;
+            while (arr[j] > pivot) j--;
+            if (i <= j) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j) sort(arr, low, j);
+        if (i < high) sort(arr, i, high);
+    }
+}
