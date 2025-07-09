02e60bdd4f365f6b2fe09502ea34c2e9095da48a
Core-007 <144080167+Core-007@users.noreply.github.com>
2025-07-09 23:45:32 +0700

    Update TwoDimensionalShape.java

--- Week8/Shapes/TwoDimensionalShape.java
+++ Week8/Shapes/TwoDimensionalShape.java
@@ -1,5 +1,9 @@
 //Lớp con của Shape cho hình 2D
 
 public abstract class TwoDimensionalShape extends Shape {
-    // Chứa các hình học 2D như Circle, Square, Triangle
+    // Lớp này sẽ chứa phương thức getArea() để tính diện tích của các hình 2D.
+    // Phương thức này sẽ được các lớp con như Circle, Square, Triangle kế thừa và cài đặt
+
+    @Override
+    public abstract double getArea();  // Diện tích của hình 2D
 }
