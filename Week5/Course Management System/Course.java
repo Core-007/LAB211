cb3ecce21014e77000fdba2f56340ea958f2a492
Core-007 <144080167+Core-007@users.noreply.github.com>
2025-07-09 18:22:07 +0700

    Create Course.java

---
+++ Week5/Course Management System/Course.java
@@ -0,0 +1,25 @@
+//Lớp cơ sở
+public class Course {
+    protected String courseID;
+    protected String courseName;
+    protected int credits;
+
+    public Course(String courseID, String courseName, int credits) {
+        this.courseID = courseID;
+        this.courseName = courseName;
+        this.credits = credits;
+    }
+
+    // Getter methods
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+}
