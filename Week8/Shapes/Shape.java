9fd088b0ea55f21e24036544d7efa79cc50ec149
Core-007 <144080167+Core-007@users.noreply.github.com>
2025-07-09 23:16:16 +0700

    Create Shape.java

---
+++ Week8/Shapes/Shape.java
@@ -0,0 +1,9 @@
+//Lớp cha
+
+public abstract class Shape {
+    public abstract double getArea();  // Diện tích
+
+    public double getVolume() {  // Thể tích (dành cho hình 3D)
+        return 0;
+    }
+}
