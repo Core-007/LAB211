255947a2d0a5ddd741d67b7df5f52b8aed1ac57b
Core-007 <144080167+Core-007@users.noreply.github.com>
2025-07-09 23:18:07 +0700

    Create ThreeDimensionalShape.java

---
+++ Week8/Shapes/ThreeDimensionalShape.java
@@ -0,0 +1,9 @@
+//Lớp con của Shape cho hình 3D
+
+public abstract class ThreeDimensionalShape extends Shape {
+    @Override
+    public abstract double getArea();  // Diện tích của hình 3D
+
+    @Override
+    public abstract double getVolume();  // Thể tích của hình 3D
+}
